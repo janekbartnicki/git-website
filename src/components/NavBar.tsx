1a9c9a188d5a2cef16c8f6151ef848becbc83136
janekbartnicki <jan.bartnicki01@gmail.com>
2023-07-05 13:56:43 +0200

    fixing navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -16,12 +16,9 @@ const NavBar: React.FC<NavBarProps> = props => {
         } else return null;
     }
 
-    // const dropdownRef = useRef<HTMLUListElement | null>(null);
-
-    // // const hideDropdown = () => {
-    // //     // naprawić
-    // //     dropdownRef.current?.setAttribute('class', 'hidden');
-    // // }
+    const hideDropdown = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
+        event.currentTarget.blur();
+    }
 
     return (
         <div className="navbar z-10 sticky top-0 glass bg-gray-100 shadow-lg">
@@ -33,8 +30,8 @@ const NavBar: React.FC<NavBarProps> = props => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu z-10 menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-30">
-                        <li><Link to='/'>Strona Główna</Link></li>
-                        <li><Link to='/sklep'>Sklep</Link></li>
+                        <li><Link to='/' onClick={hideDropdown}>Strona Główna</Link></li>
+                        <li><Link to='/sklep' onClick={hideDropdown}>Sklep</Link></li>
                         {/* <li>
                         <a>Parent</a>
                         <ul className="p-2">
@@ -57,8 +54,8 @@ const NavBar: React.FC<NavBarProps> = props => {
             {/* desktop */}
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1 text-xl justify-around">
-                    <li><Link to='/'>Strona Główna</Link></li>
-                    <li><Link to='/sklep'>Sklep</Link></li>
+                    <li><Link to='/' onClick={hideDropdown}>Strona Główna</Link></li>
+                    <li><Link to='/sklep' onClick={hideDropdown}>Sklep</Link></li>
                     {/* <li tabIndex={0}>
                         <details>
                         <summary>Sklep</summary>
--- src/components/ShortAbout.tsx
+++ src/components/ShortAbout.tsx
@@ -1,11 +1,22 @@
+import { Link } from 'react-router-dom';
+import { GrNext } from 'react-icons/gr';
+
 const ShortAbout: React.FC = () => {
     return (
         <div className="flex flex-wrap justify-center justify-items-center justify-self-center lg:md:flex-nowrap lg:m-20 m-10">
             <img draggable={false} src="images/marcin_fec1.png" className="rounded-lg drop-shadow-2xl lg:max-h-[35vw] sm:max-h-[60vw]"/>
             {/* TODO: ogarnąć skalowanie się tekstu */}
-            <div className="m-10">
+            <div className="lg:md:m-10 m-3 lg:md:my-10 my-10">
                 <h1 className="font-bold text-5xl">Marka stworzona przez Marcina Feć</h1><br/>
                 <p className="text-2xl">Marka GiT została stworzona przez wieloletniego zawodnika oraz trenera <b>Marcina Feć</b>. Grał on w takich klubach jak <b>Odra Opole</b> czy <b>Piast Gliwice</b>. Aktualnie prowadzi własną szkółkę bramkarską oraz zasila sztab szkoleniowy pierwszoligowej OKS Odry Opole jako trener bramkarzy.<br/><br/>W oparciu o doświadczenia między słupkami zaprojketował on serię <b>najwyższej jakości</b> rękawic bramkarskich. Oprócz znakomitych osiągów produkt cechuje także świetny wygląd i ergonomia.</p>
+                <div className='hidden xl:block'>
+                    <div className='flex justify-center my-24'>
+                        <button className='btn bg-[#3d61aa] lg:md:w-64 text-white group'>
+                            <Link to='/'><p className='group-hover:text-black'>Więcej o Nas</p></Link>
+                            <GrNext className="group-hover:invert-0 invert"/>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
         // <div className="flex flex-row lg:flex-nowrap md:sm:flex-wrap justify-center justify-items-center justify-self-center lg:m-20 m-10">
