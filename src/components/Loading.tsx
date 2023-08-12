d6eab3a8914a905fd00ea7193402c6ce5561533a
janekbartnicki <jan.bartnicki01@gmail.com>
2023-08-12 22:27:21 +0200

    working on loading screen

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,9 +1,16 @@
 const Loading: React.FC = () => {
-    return <div className="text-center mt-72">
-        <h1 className="text-xl text-bold">Witamy!</h1><br/>
-        <span className="loading loading-spinner loading-lg"></span><br/>
-        <p className="mt-5">Trwa ładowanie storny...</p>
-    </div>
+    return (
+        <>
+            <img 
+                src="/images/icon_logo.png" 
+                alt="Logo GiT" 
+                className="max-h-20 animate-ping animate-infinite absolute z-0 top-52 m-auto left-0 right-0"
+            />
+            <div className="text-center">
+                <span className="loading loading-dots loading-lg absolute bottom-16"></span>
+            </div>
+        </>
+    )
 }
 
 export default Loading;
